2bba56030657560ca4408393698b0629fa04437c
Sergiu154 <sergiu.victor890@gmail.com>
2019-07-26 15:38:44 +0300

    Treated some corner cases

--- ftp_multithread.py
+++ ftp_multithread.py
@@ -2,37 +2,53 @@ import socket
 from threading import Thread
 
 
-def clientThread(connection):
-    is_active = True  # TO-DO implement the functionality to re-login
-    while is_active:  # meanwhile the user is permanently connected to the server until he quits
-        login(connection)
+def parse_command(m_text):
+    index = m_text.find(' ')
+    comm = m_text[0:index]
+    arg = m_text[index + 1:]
+    return comm, arg
 
 
-def login(clientsocket):
-    clientsocket.send(b'220 Welcome to ProXad FTP server\n')
-    print(1)
-    username = clientsocket.recv(4096).decode('utf-8')
-    clientsocket.send(b'331 Please specify the password\n')
-    password = clientsocket.recv(4096).decode('utf-8')
-    print(username, end='')
-    print(password)
-    clientsocket.send(b'430 Invalid username or password\n')
+def request_comm(clientsocket, one_string):
+    clientsocket.send(one_string)
+    arg = clientsocket.recv(4096).decode('utf-8')
+    return parse_command(arg)
+
+
+def clientThread(clientsocket, ip, prt):
+    comm_user, user = request_comm(clientsocket, b'220 Welcome to ProXad FTP server\n')
+
+    if comm_user.upper() != 'USER':
+        print(str(ip) + ' ' + str(prt) + ' ' + 'UNKNOWN' + ' ' + comm_user + ' ' + user.strip('\n') + '\n')
+        clientsocket.close()
+    else:
+        comm_pass, paswd = request_comm(clientsocket, b'331 Please specify the password\n')
+
+        message = 'LOGIN'
+
+        if comm_pass.upper() != 'PASS':
+            message = "UNKNOWN"
+            print(str(ip) + ' ' + str(prt) + ' ' + message + ' ' + comm_pass + ' ' + paswd.strip('\n') + '\n')
+
+        else:
+            print(str(ip) + ' ' + str(prt) + ' ' + message + ' ' + user.strip('\n') + ' ' + paswd.strip('\n') + '\n')
+        clientsocket.send(b'430 Invalid username or password\n')
+        clientsocket.close()
 
 
 def main():
     port = 21
-    socketserver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socketserver.bind(('', port))
-    socketserver.listen(1000)
-    while True:
-        clientsocket, address = socketserver.accept()
-        ip, prt = address
-        print('Client address is ' + str(ip) + ' ' + str(prt))
-        try:
-            Thread(target=clientThread, args=(clientsocket,)).start()
-        except:
-            print('Thread did not start.')
-    socketserver.close()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socketserver:
+        socketserver.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        socketserver.bind(('', port))
+        socketserver.listen(1000)
+        while True:
+            clientsocket, address = socketserver.accept()
+            ip, prt = address
+            try:
+                Thread(target=clientThread, args=(clientsocket, ip, prt)).start()
+            except:
+                print('Thread did not start.')
 
 
 if __name__ == '__main__':
