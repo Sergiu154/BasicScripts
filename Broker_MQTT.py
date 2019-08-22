d2bb2078dc5a8b346f0f83e4b287cab638811c3c
Sergiu <sergiu.victor890@gmail.com>
2019-08-22 12:50:38 +0300

    Added a client class to store the data,implemented publish and subscribe handlers

--- Broker_MQTT.py
+++ Broker_MQTT.py
@@ -7,6 +7,48 @@ import json
 
 logging.basicConfig(level=logging.DEBUG)
 
+clients_dict = {}
+
+
+class ClientData:
+    def __init__(self, ip=' ', prt='', user='', pswd='', client_id=''):
+        self.ip = ip
+        self.port = prt
+        self.password = pswd
+        self.username = user
+        self.client_id = client_id
+        self.subscribed_topics = []
+        self.message_on_topic = {}
+
+    def set_password(self, passwd):
+        self.password = passwd
+
+    def set_ip(self, IP):
+        self.ip = IP
+
+    def set_username(self, user):
+        self.username = user
+
+    def set_port(self, prt):
+        self.port = prt
+
+    def add_topic(self, tpc, qos):
+        if (tpc, qos) not in self.subscribed_topics:
+            self.subscribed_topics.append((tpc, qos))
+
+    # TO-DO: edge case: client publishes on a topic on which he is not subscribed
+    def add_message_on_topic(self, tpc, msg):
+        if not self.message_on_topic.get(tpc):
+            self.message_on_topic[tpc] = []
+        self.message_on_topic[tpc].append(msg)
+
+    def print_client(self):
+        print(self.username, self.password, self.ip, self.port, self.client_id)
+        for topic in self.subscribed_topics:
+            print(topic)
+        for key in self.message_on_topic.keys():
+            print(self.message_on_topic.get(key))
+
 
 # get a string o bits from an integer (little-endian)
 
@@ -48,6 +90,7 @@ def decimal_from_n_bytes(integers_list):
 
 len_and_ints = 'LEN_AND_INT'
 len_and_string = 'LEN_AND_STRING'
+msgID_and_msg = 'ID_AND_MESSAGE'
 
 
 # interprets a (field_len,field_value) pattern present in a CONNECT packet
@@ -58,26 +101,38 @@ def get_field_len_and_value(command, index, combo_type):
 
     index += 2
     field_value = ''
-    for i in range(0, length):
-        field_value += chr(command[index + i])
 
-    index += length
-
-    if combo_type == 'LEN_AND_INT':
-        return index, length, int(field_value)
-    elif combo_type == 'LEN_AND_STRING':
+    if combo_type == 'ID_AND_MESSAGE':
+        while True:
+            try:
+                field_value += chr(command[index])
+                index += 1
+            except IndexError as e:
+                break
         return index, length, field_value
+    else:
+        for i in range(0, length):
+            field_value += chr(command[index + i])
+        index += length
+        if combo_type == 'LEN_AND_INT':
+            return index, length, int(field_value)
+        elif combo_type == 'LEN_AND_STRING':
+            return index, length, field_value
 
 
 def connect_comm(command, client):
     # send Connack packet
 
-    sess_present = 0
-
-    # TODO check it the client is in your database(maybe a list of clients)
+    # TODO check if the client is in your database(maybe a list of clients)
     #  if True: sessPresent =1
 
+    sess_present = 0
+
     client.send(bytes(MQTT() / MQTTConnack(sessPresentFlag=sess_present, retcode=0)))
+
+    ip, port = client.getpeername()
+
+    logging.debug('Client connected: ' + ip + ' ' + str(port))
 
     # jump over msg len, protocol name and length
     index = pass_message_len(command) + 8
@@ -109,7 +164,7 @@ def connect_comm(command, client):
 
     index, client_id_length, client_id = get_field_len_and_value(command, index, len_and_ints)
 
-    logging.debug('Client id is : ' + str(client_id))
+    logging.debug('Client ID: ' + str(client_id))
 
     # go to the payload whose structure depends on the will_flag
 
@@ -129,55 +184,60 @@ def connect_comm(command, client):
         if username_flag and password_flag:
             index, username_len, username = get_field_len_and_value(command, index, len_and_string)
             index, password_len, password = get_field_len_and_value(command, index, len_and_string)
-            logging.debug('Username len and user: ' + str(username_len) + ' ' + username)
-            logging.debug('Password len and password: ' + str(password_len) + ' ' + password)
+            logging.debug('Username: ' + username)
+            logging.debug('Password: ' + password)
 
     # TODO use a data structure to store a new incoming client
     #  and handle the CleanSession scenario
 
-    # return a Connect packet as a string
-    pkt = MQTTConnect(usernameflag=username_flag, passwordflag=password_flag, willretainflag=will_retain,
-                      willQOSflag=will_qos, willflag=will_flag, cleansess=int(connect_flag[6]),
-                      reserved=int(connect_flag[7]),
-                      klive=keep_alive, clientIdlen=client_id_length, clientId=client_id,
-                      wtoplen=will_topic_len,
-                      willtopic=will_topic, wmsglen=will_msg_len, willmsg=will_msg, userlen=username_len,
-                      username=username, passlen=password_len, password=password)
-    return pkt.show(dump=True)
+    return ClientData(ip=ip, prt=port, user=username, pswd=password, client_id=str(client_id))
 
 
-def publish_comm(command, client, header_as_bits):
+def publish_comm(command, client, header_as_bits, client_data):
     qos = int(header_as_bits[1]) + 2 * int(header_as_bits[2])
     logging.debug('QOS ' + str(qos))
 
     index = pass_message_len(command) + 1
 
+    # get topic and message
     index, topic_len, topic = get_field_len_and_value(command, index, len_and_string)
-    index, msg_id, message = get_field_len_and_value(command, index, len_and_string)
+    index, msg_id, message = get_field_len_and_value(command, index, msgID_and_msg)
 
+    # store the data that has been received
+    client_data.add_topic(topic, qos)
+    client_data.add_message_on_topic(topic, message)
+
+    # send the right response packet depending on the QoS level
     if qos == 1:
         client.send(bytes(MQTT() / MQTTPuback(msgid=msg_id)))
     elif qos == 2:
         client.send(bytes(MQTT() / MQTTPubrec(msgid=msg_id)))
         client.send(bytes(MQTT() / MQTTPubcomp(msgid=msg_id)))
-    pkt = MQTTPublish(length=int(topic_len), topic=topic, msgid=msg_id, value=message)
-    # return pkt.show(dump=True)
+    # client.send(message.encode('utf-8'))
 
 
-def subscribe_comm(command, client, header):
-    granted_qos = decimal_from_n_bytes([command[len(command) - 1]])
-    logging.debug('Granted QoS: ' + str(granted_qos))
-
+def subscribe_comm(command, client, client_data):
     index = pass_message_len(command) + 1
 
     msg_id = decimal_from_n_bytes([command[index + 1], command[index]])
+
+    index += 2
+    while True:
+        try:
+            index, topic_len, topic = get_field_len_and_value(command, index, len_and_string)
+            granted_qos = decimal_from_n_bytes([command[index]])
+            client_data.add_topic(topic, granted_qos)
+            logging.debug('Granted QoS: ' + str(granted_qos))
+            index += 1
+        except IndexError:
+            break
 
     client.send(bytes(MQTT() / MQTTSuback(msgid=msg_id, retcode=granted_qos)))
 
 
 def disconnect_command(command, client, header, client_packet):
     with open('data.txt', 'w') as fwrite:
-        json.dump(client_packet, fwrite, indent=4)
+        json.dump(client_packet.__dict__, fwrite, indent=4)
 
 
 def client_thread(client, command_type):
@@ -192,19 +252,18 @@ def client_thread(client, command_type):
                 # check if reserved field is 0, otherwise close the connection
                 if header_as_bits[:4] != '0000':
                     client.close()
-                connect_packet = connect_comm(command, client)
-                logging.debug(connect_packet)
+                client_data = connect_comm(command, client)
 
             elif type_of_command == 'PUBLISH':
-                publish_comm(command, client, header_as_bits)
+                publish_comm(command, client, header_as_bits, client_data)
 
             elif type_of_command == 'SUBSCRIBE':
 
-                subscribe_comm(command, client, header_as_bits)
+                subscribe_comm(command, client, client_data)
 
             elif type_of_command == 'DISCONNECT':
 
-                disconnect_command(command, client, header_as_bits, connect_packet)
+                disconnect_command(command, client, header_as_bits, client_data)
 
                 break
     client.close()
